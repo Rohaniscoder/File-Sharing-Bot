964e2b37606cf3a6226018c94506fb263d8a56b7
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2023-10-06 09:45:28 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/Cinema_Moviesz:'>This Person </a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Rohaniscoder/File-Sharing-Bot'>Click here</a>\n○ Channel : @Cinema_Moviesz\n○ Support Group <a href='https://t.me/+zspL8ojzHNYxNDVl'>Click Here</a> </b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Rohaniscoder/File-Sharing-Bot'>Click here</a>\n○ Channel : @Cinema_Moviesz\n○ Support Group <a href='https://t.me/+zspL8ojzHNYxNDVl'>Click Here</a> </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
